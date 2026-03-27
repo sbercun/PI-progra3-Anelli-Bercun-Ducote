5d8762e214847e72db27c0bf4457f502a4f6a8c7
sbercun <sbercun@udesa.edu.ar>
2026-03-27 10:58:27 -0300

    Arme las rutas/paths en app.js' git status

--- proyecto-integrador-tdm/src/App.js
+++ proyecto-integrador-tdm/src/App.js
@@ -1,24 +1,47 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Home from "./screens/Home/Home";
+import Series from "./screens/Series/Series";
+import Favoritos from "./screens/Favoritos/Favoritos";
+import LogIn from "./screens/LogIn/LogIn";
+import CrearCuenta from "./screens/CrearCuenta/CrearCuenta";
+import NotFound from "./screens/NotFound/NotFound";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <React.Fragment>
+      <Header />
+      <Switch>
+      <Route path="/" exact={true} component={Home}/>
+        <Route path="/peliculas" component={Peliculas}/>
+        <Route path="/series" component={Series}/>
+        <Route path="/favoritos" component={Favoritos}/>
+        <Route path="/logIn" component={LogIn}/>
+        <Route path="/signIn" component={CrearCuenta}/>
+        <Route path="*" component={NotFound}/>
+      </Switch>
+    </React.Fragment>
+    
+    //<div className="App">
+      //<header className="App-header">
+       // <img src={logo} className="App-logo" alt="logo" />
+        //<p>
+         // Edit <code>src/App.js</code> and save to reload.
+        //</p>
+        //<a
+         // className="App-link"
+          //href="https://reactjs.org"
+          //target="_blank"
+          //rel="noopener noreferrer"
+       // >
+       //   Learn React
+       // </a>
+     // </header>
+   // </div>
   );
 }
 
--- proyecto-integrador-tdm/src/components/Menu/Menu.js
+++ proyecto-integrador-tdm/src/components/Menu/Menu.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 function Menu(){
     return(
-        <div className='menu'>
-            <h1>Menu</h1>
-        </div>
+        <ul className='menu'>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/peliculas">Peliculas</Link></li>
+            <li><Link to="/series">Series</Link></li>
+            <li><Link to="/favoritos">Favoritos</Link></li>
+            <li><Link to="/logIn">LogIn</Link></li>
+            <li><Link to="/signIn">SignIn</Link></li>
+        </ul>
     )
 }
 
