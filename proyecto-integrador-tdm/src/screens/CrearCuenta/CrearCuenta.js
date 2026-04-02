eda4bb0da3a2e1f21413615166ad70e3a111e523
juliaanelli4 <janellicerruti@udesa.edu.ar>
2026-04-02 12:05:41 -0300

    Cambios en CrearCuenta.js

--- proyecto-integrador-tdm/src/screens/CrearCuenta/CrearCuenta.js
+++ proyecto-integrador-tdm/src/screens/CrearCuenta/CrearCuenta.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from '../../components/Header/Header';
- 
-function CrearCuenta(){
-    return(
-        <React.Fragment>
-            <h2>crear cuenta</h2>
-        </React.Fragment>
-    )
 
+class CrearCuenta extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            error: ""
+        };
+    }
+
+
+    evitarSubmit(event) {
+        event.preventDefault();
+    }
+
+    controlarEmail(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    controlarPassword(event) {
+        this.setState({ password: event.target.value });
+    }
+
+
+    render() {
+        return (
+            <div>
+
+                <React.Fragment>
+                    <h2>Crear cuenta</h2>
+                </React.Fragment>
+
+
+                <form onSubmit={(event) => this.evitarSubmit(event)}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={(event) => this.controlarEmail(event)} />
+
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={this.state.password}
+                        onChange={(event) => this.controlarPassword(event)} />
+
+                    <button type="submit">Crear cuenta</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 
- export default CrearCuenta;
+
+
+export default CrearCuenta;
+
