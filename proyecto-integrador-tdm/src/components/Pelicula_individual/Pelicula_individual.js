13eac27db955238ba08dec11a0bcf8e6a6eb5aa0
sbercun <sbercun@udesa.edu.ar>
2026-04-06 17:44:02 -0300

    Ahora cuando se recarga la pagina no se borra el estado de la card dxe si ya esta en favoritos o hay que eliminarla

--- proyecto-integrador-tdm/src/components/Pelicula_individual/Pelicula_individual.js
+++ proyecto-integrador-tdm/src/components/Pelicula_individual/Pelicula_individual.js
@@ -29,6 +29,7 @@ agregarFav(id){
   if (storageRecuperado == null) {
     let primerValor = [id];
     let primerValorString = JSON.stringify(primerValor)
+    localStorage.setItem('favPeliculas', primerValorString)
   } else{ //casos que devuelva un array con datos
     storageRecuperado.push( id )
     let storageString = JSON.stringify( storageRecuperado )
@@ -53,19 +54,31 @@ sacarFav(id){
   //cambio el estado a False para que aparezca el boton de agregar a favoritos
   this.setState( {esFav: false} )
 }
-    render(){
-        return(
-            <article className='pelicula'>
-                <img src={this.props.imagen} alt="" />
-                <h2>{this.props.nombre} </h2> 
-                <p className={this.state.VerDesc ? "mostrar" : "ocultar"}>{this.props.descripcion}</p>
-                <button onClick={() => this.state.VerDesc ? this.ocultar() : this.mostrar()}> {this.state.VerDesc ? "Ver menos" : "Ver mas"}</button>
-                <button onClick={() => this.state.esFav ? this.sacarFav() : this.agregarFav()}> {this.state.esFav ? "Sacar de favoritos" : "Agregar a favoritos"}</button>
-                <Link to={`/Detalle/${this.props.id}`}>Ir a detalle</Link>
-            </article>
 
-        )
-    }
+componentDidMount(){
+  //tengo que hacer un if para que cuando recargue la pagina, se mantenga el sacar de favoritos o agregar a favoritos
+  let peliculasGuardadas = localStorage.getItem('favPeliculas');
+  let storageRecuperado = JSON.parse(peliculasGuardadas)
+  
+  if (storageRecuperado !== null) {
+    let esFavorito = storageRecuperado.filter(peliculaId => peliculaId === this.props.id);
+    this.setState({ esFav: esFavorito.length > 0 });
+  }
+}
+
+render(){
+    return(
+        <article className='pelicula'>
+            <img src={this.props.imagen} alt="" />
+            <h2>{this.props.nombre} </h2> 
+            <p className={this.state.VerDesc ? "mostrar" : "ocultar"}>{this.props.descripcion}</p>
+            <button onClick={() => this.state.VerDesc ? this.ocultar() : this.mostrar()}> {this.state.VerDesc ? "Ver menos" : "Ver mas"}</button>
+            <button onClick={() => this.state.esFav ? this.sacarFav(this.props.id) : this.agregarFav(this.props.id)}> {this.state.esFav ? "Sacar de favoritos" : "Agregar a favoritos"}</button>
+            <Link to={`/Detalle/${this.props.id}`}>Ir a detalle</Link>
+        </article>
+
+    )
+}
     
 }
 export default Pelicula_individual;
--- proyecto-integrador-tdm/src/screens/Favoritos/Favoritos.js
+++ proyecto-integrador-tdm/src/screens/Favoritos/Favoritos.js
@@ -6,26 +6,25 @@ class Favoritos extends Component{
     constructor(props){
         super(props);
         // Defino el estado inicial de los componentes para peliculas y series
-        this.state = {
-            pelisFav: [],
-            seriesFav: []
+        this.state = {  
+            infoFav: {}, //con llaves xq la api me devuelve un objeto liyetal
         };
     }
     
     componentDidMount(){
-        // Busco en  el localStorage si ya hay películas y series guardadas como favoritas
-        let peliculasGuardadas = localStorage.getItem('pelisFav');
-        let seriesGuardadas = localStorage.getItem('seriesFav');
-
-        // Actualizo el estado con las películas y series favoritas guardadas en localStorage
-            // Si existen datos, los convierto de string a array con JSON.parse()
-            // Si no existen, se deja un array vacío 
-        this.setState({
-            pelisFav: peliculasGuardadas ? JSON.parse(peliculasGuardadas) : [],
-            seriesFav: seriesGuardadas ? JSON.parse(seriesGuardadas) : []
-
-
-        });
+        let peliculasGuardadas = localStorage.getItem('favPeliculas');
+        let storageRecuperado = JSON.parse(peliculasGuardadas);
+        
+        storageRecuperado.map( pelicula => {fetch(`https://api.themoviedb.org/3/movie/${pelicula}`)
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                infoFav: data
+            });
+            console.log("heloooo");
+        })
+        .catch((error) => console.log(error))}
+        )
     }
 
     render(){
@@ -34,10 +33,7 @@ class Favoritos extends Component{
                 <h1>Mis Favoritos</h1>
 
                 <section>
-
-                {/* tengo que armar un if ternario de que si SI hay peliculas guardadas como favoritas en el localStorage, entonces que se me aparezcan, sino dejar el array vacio */}                    {/* lo que apareceria dentro debo sacarlo de los components de peliculas y series. */}
-                    {/* TODO NECESITA COOKIES */}
-
+                    <Pelicula_individual />
                 </section>
             </div>
         )
