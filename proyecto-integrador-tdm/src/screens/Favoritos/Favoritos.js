2412b888dacc8c834cbc12b9cb2c1cc8ecbc3638
sbercun <sbercun@udesa.edu.ar>
2026-04-09 15:31:28 -0300

    Termine favoritos

--- proyecto-integrador-tdm/src/screens/Favoritos/Favoritos.js
+++ proyecto-integrador-tdm/src/screens/Favoritos/Favoritos.js
@@ -14,26 +14,45 @@ class Favoritos extends Component{
     componentDidMount(){
         let peliculasGuardadas = localStorage.getItem('favPeliculas');
         let storageRecuperado = JSON.parse(peliculasGuardadas);
+    
+        if (storageRecuperado == null) {
+            storageRecuperado = [];
+        }
+    
         let arrayPeliculas = [];
-
-        storageRecuperado.map( pelicula => {fetch(`https://api.themoviedb.org/3/movie/${pelicula}`)
-        .then((response) => response.json())
-        .then((data) => {
-            arrayPeliculas.push(data);
-        
-            console.log("heloooo");
-        })
-        .catch((error) => console.log(error))}
-        )
+    
+        storageRecuperado.map(pelicula => {
+            fetch(`https://api.themoviedb.org/3/movie/${pelicula}`,
+            {method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTE0MmM2Y2E4ZmM3MTg2OTM0NDU1MDQ2ZGM3NjM2OSIsIm5iZiI6MTc3NDU1OTQ0Mi4xMzY5OTk4LCJzdWIiOiI2OWM1YTBkMmIwNjQ3OWNkYjQ0MjFiMGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.o5IT_ep0A_LgC8mJgaQmE9oW3CTTtgeN52AN9v8FlyE"
+            }}
+            )
+            .then((response) => response.json())
+            .then((data) => {
+                arrayPeliculas.push(data);
+    
+                this.setState({ infoFav: arrayPeliculas }); // ← clave
+            })
+            .catch((error) => console.log(error))
+        });
     }
 
     render(){
         return(
             <div>
                 <h1>Mis Favoritos</h1>
-
                 <section>
-                    <Pelicula_individual />
+                    {this.state.infoFav.map(peli => (
+                    <Pelicula_individual
+                    key={peli.id}
+                    id={peli.id}
+                    imagen={`https://image.tmdb.org/t/p/w500${peli.poster_path}`}
+                    nombre={peli.title}
+                    descripcion={peli.overview}
+                    />
+                    ))}
                 </section>
             </div>
         )
