8c563c038eda8d6364a5c5f5cd719d73557ba090
sbercun <sbercun@udesa.edu.ar>
2026-04-13 17:22:02 -0300

    Agregue la cookie en el header para favoritos

--- proyecto-integrador-tdm/src/components/Menu/Menu.js
+++ proyecto-integrador-tdm/src/components/Menu/Menu.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+const sesion = cookies.get("user-auth-cookie");
 
 function Menu() {
+
     return (
         <ul className="menu">
             <li className="menu-item">
                 <Link className="menu-link" to="/">Home</Link>
             </li>
+            
+            {/* si no hay sesion mostrar LogIn y Crear cuenta  */}
+            {!sesion && 
+                <li className="menu-item">
+                    <Link className="menu-link" to="/logIn">LogIn</Link>
+                </li>
+            }
+            {!sesion && 
+                <li className="menu-item">
+                    <Link className="menu-link" to="/signIn">SignIn</Link>
+                </li>
+            }
 
-            <li className="menu-item">
-                <Link className="menu-link" to="/favoritos">Favoritos</Link>
-            </li>
-
-            <li className="menu-item">
-                <Link className="menu-link" to="/logIn">LogIn</Link>
-            </li>
-
-            <li className="menu-item">
-                <Link className="menu-link" to="/signIn">SignIn</Link>
-            </li>
+            {/* Si sí hay sesion, que muestre vaforitos */}
+            {sesion && 
+                <li className="menu-item">
+                    <Link className="menu-link" to="/favoritos">Favoritos</Link>
+                </li>
+            }
         </ul>
     );
 }
