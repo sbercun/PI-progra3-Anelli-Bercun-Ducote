7a8dd6c1fa27fdadec64df07b3878059ab329918
MoraDucote <mora@Moras-MacBook-Air-2.local>
2026-05-14 15:58:33 -0300

    actividad de hooks en clase

--- proyecto-integrador-tdm/src/components/Peliculas_popular/Peliculas_popular.js
+++ proyecto-integrador-tdm/src/components/Peliculas_popular/Peliculas_popular.js
@@ -1,41 +1,32 @@
 import React, { Component } from "react";
 import Item_individual from "../Item_individual/Item_individual";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-class Peliculas_popular extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      array_pelicula: []
-    };
-  }
+function Peliculas_popular(props) {
 
-  componentDidMount() {
-    fetch("https://api.themoviedb.org/3/movie/popular" ,
-        {method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTE0MmM2Y2E4ZmM3MTg2OTM0NDU1MDQ2ZGM3NjM2OSIsIm5iZiI6MTc3NDU1OTQ0Mi4xMzY5OTk4LCJzdWIiOiI2OWM1YTBkMmIwNjQ3OWNkYjQ0MjFiMGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.o5IT_ep0A_LgC8mJgaQmE9oW3CTTtgeN52AN9v8FlyE"
-        }})
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          array_pelicula: data.results
-        });
-      })
-      .catch((error) => console.log(error));
-  }
+  const[peliculas, setPeliculas]= useState([])
 
-  render() {
+  useEffect( () => {
+      fetch("https://api.themoviedb.org/3/movie/popular" ,
+          {method: "GET",
+              headers: {
+                  accept: "application/json",
+                  Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTE0MmM2Y2E4ZmM3MTg2OTM0NDU1MDQ2ZGM3NjM2OSIsIm5iZiI6MTc3NDU1OTQ0Mi4xMzY5OTk4LCJzdWIiOiI2OWM1YTBkMmIwNjQ3OWNkYjQ0MjFiMGQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.o5IT_ep0A_LgC8mJgaQmE9oW3CTTtgeN52AN9v8FlyE"
+          }})
+        .then((response) => response.json())
+        .then((data) => {setPeliculas(data.results)})
+        .catch((error) => console.log(error));
+    } , [])
+
     return (
       <section className="pelicula-popular">
         <div className="container_peliculas">
-          {this.state.array_pelicula.slice(0,6).map((pelicula) => (<Item_individual key={pelicula.id} id={pelicula.id} imagen={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} nombre={pelicula.title} descripcion={pelicula.overview} tipo="movie"/>))}
+          {peliculas.slice(0,6).map((pelicula) => (<Item_individual key={pelicula.id} id={pelicula.id} imagen={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} nombre={pelicula.title} descripcion={pelicula.overview} tipo="movie"/>))}
         </div>
         <Link className="boton-ver-todas" to="/peliculas_populares">Ver todas</Link>
       </section>
     );
   }
-}
 
 export default Peliculas_popular;
